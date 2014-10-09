da69796d727833761908b7b77e8ca314857d1da3
Jody Tate <jody@josephtate.com>
2014-10-08 21:03:56 -0700

    second commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-angular-doge-directive
-======================
+# Angular Doge Directive
+[Homepage](http://jodytate.github.com/angular-doge-directive/)
+
+## Bower
+Installable via `bower`:
+
+```bash
+bower install angular-doge-directive
+```
+
+## Example
+See the [homepage](http://jodytate.github.com/angular-doge-directive/) for an example.
+
+```html
+<div ng-app="doge-directive">
+  <doge></doge>
+</div>
+```
+
+## Credits
+Angular Doge Directive was inspired by [Angular Unicorn Directive](https://github.com/btford/angular-unicorn-directive).
+
+## License
+MIT
---
+++ doge.js
@@ -0,0 +1,33 @@
+
+
+
+var link = function (scope, element, attr) {
+  var baseWidth = 550;
+  var baseHeight = 550;
+  var canvas = element.find('canvas')[0];
+  var context = canvas.getContext('2d');
+  var options = {
+    scale:  attr.scale  || 1.0,
+    width:  attr.width  || baseWidth,
+    height: attr.height || baseHeight,
+  };
+
+  var doge = new Image();
+  doge.src = "img/doge.png"; 
+
+  canvas.width = options.width * options.scale;
+  canvas.height = options.height * options.scale;
+  
+  doge.onload = function() {
+     context.drawImage(doge, 0, 0, canvas.width, canvas.height);
+  };
+}
+
+angular.module('doge-app', []).
+  directive('doge', function () {
+    return {
+      restrict: 'E',
+      template: '<canvas></canvas>',
+      link: link
+    };
+  });
